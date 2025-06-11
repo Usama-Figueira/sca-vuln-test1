ef1118e39bc74aa8f81754461e439dbb23a9b3c5
Usama-Figueira <usama@figueiraonline.com>
2025-06-11 20:48:31 +0500

    Update ReachabilityTest.java

--- src/test/java/com/example/ReachabilityTest.java
+++ src/test/java/com/example/ReachabilityTest.java
@@ -7,7 +7,7 @@ public class ReachabilityTest {
     @Test
     public void testJacksonVuln() throws Exception {
         System.out.println("[Test Code] Triggering Jackson deserialization vuln...");
-        String json = "["java.lang.Exception","Exploit"]";
+        String json = "[\"java.lang.Exception\",\"Exploit\"]";
         ObjectMapper mapper = new ObjectMapper();
         Object obj = mapper.readValue(json, Object.class);
         System.out.println("Deserialized: " + obj);
