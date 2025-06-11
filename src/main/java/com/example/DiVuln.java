fccb07c4f300bcfffa4548148d065686dce7088e
Usama-Figueira <usama@figueiraonline.com>
2025-06-11 20:49:04 +0500

    Update DiVuln.java

--- src/main/java/com/example/DiVuln.java
+++ src/main/java/com/example/DiVuln.java
@@ -5,6 +5,6 @@ import org.jsoup.Jsoup;
 public class DiVuln {
     public static void execute() {
         String maliciousInput = "<img src=x onerror=alert('XSS')>";
-        System.out.println("JSoup output: " + Jsoup.clean(maliciousInput, "", org.jsoup.safety.Safelist.none(), new org.jsoup.nodes.Document.OutputSettings().prettyPrint(false)));
+        System.out.println("JSoup output: " + Jsoup.clean(maliciousInput, "", org.jsoup.safety.Whitelist.none(), new org.jsoup.nodes.Document.OutputSettings().prettyPrint(false)));
     }
 }
