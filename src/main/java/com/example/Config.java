93eb6be6dea3262bc1d78ac71391eedaad50bc89
Usama-Figueira <usama@figueiraonline.com>
2025-06-11 20:01:47 +0500

    Update Config.java

--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -1,5 +1,6 @@
 package com.example;
 
 public class Config {
-    public static final boolean FEATURE_FLAG = Boolean.parseBoolean(System.getenv("FEATURE_FLAG"));
+//    public static final boolean FEATURE_FLAG = Boolean.parseBoolean(System.getenv("FEATURE_FLAG"));
+    public static final boolean FEATURE_FLAG = false;
 }
